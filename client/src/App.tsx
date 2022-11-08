910e22a080bb9693798f65beca1961a95cf39dd4
Gabriel Marcano <gabrielmarcano120@gmail.com>
2022-11-07 22:58:19 -0400

    client: simple socket connect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const connect = () => {
+    const socket = io('http://localhost:9000')
+  }
+
+  useEffect(() => {
+    connect()
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-3xl font-bold underline'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="text-center">
+      <h1 className='underline text-4xl'>Guess Who Game</h1>
     </div>
   )
 }
