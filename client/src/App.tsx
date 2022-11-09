0a7f22ddde5a26bff6921653e63be50658785f71
Gabriel Marcano <gabrielmarcano120@gmail.com>
2022-11-08 22:34:43 -0400

    client: use game context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react'
-import { io } from 'socket.io-client'
 import { JoinRoom } from './components/joinRoom'
+import GameContext, { IGameContextProps } from './gameContext'
 import socketService from './services/socketService'
 
 function App() {
+  const [isInRoom, setIsInRoom] = useState(false)
 
   const connectSocket = async () => {
-    const socket = socketService.connect("http://localhost:9000").catch((err) => {
+    const socket = socketService.connect("http://192.168.0.129:9000").catch((err) => {
       console.error("Error: ", err)
     })
   }
@@ -15,13 +16,18 @@ function App() {
     connectSocket()
   }, [])
 
+  const gameContextValue: IGameContextProps = {
+    isInRoom,
+    setIsInRoom
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className='underline text-4xl'>Guess Who Game</h1>
-      <div>
+    <GameContext.Provider value={gameContextValue}>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className='mt-5 underline text-4xl'>Guess Who Game</h1>
         <JoinRoom></JoinRoom>
       </div>
-    </div>
+    </GameContext.Provider>
   )
 }
 
