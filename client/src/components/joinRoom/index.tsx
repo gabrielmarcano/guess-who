53b85e0f6eca11c4b0ba7841ec024fff6e09d024
Gabriel Marcano <gabrielmarcano120@gmail.com>
2023-01-05 09:58:32 -0400

    separate inputvalue from roomname

--- client/src/components/joinRoom/index.tsx
+++ client/src/components/joinRoom/index.tsx
@@ -7,14 +7,15 @@ interface IJoinRoomProps { }
 
 export function JoinRoom(props: IJoinRoomProps) {
 
+  const [inputValue, setInputValue] = useState('')
   const [roomName, setRoomName] = useState('')
   const [isJoining, setIsJoining] = useState(false)
 
   const { isInRoom, setIsInRoom } = useContext(gameContext)
 
-  const handleRoomNameChange = (e: React.ChangeEvent<any>) => {
+  const handleInputValueChange = (e: React.ChangeEvent<any>) => {
     const value = e.target.value
-    setRoomName(value)
+    setInputValue(value)
   }
 
   const joinRoom = async (e: React.FormEvent) => {
@@ -22,17 +23,19 @@ export function JoinRoom(props: IJoinRoomProps) {
 
     const socket = socketService.socket
 
-    if (!roomName || roomName.trim() === '' || !socket) return
+    if (!inputValue || inputValue.trim() === '' || !socket) return
 
     setIsJoining(true)
 
-    const joined = await gameService.joinGameRoom(socket, roomName).catch((err) => {
+    const joined = await gameService.joinGameRoom(socket!, inputValue).catch((err) => {
       alert(err)
     })
 
     if (joined) setIsInRoom(true)
 
     setIsJoining(false)
+
+    setRoomName(inputValue)
   }
 
   return (
@@ -44,7 +47,7 @@ export function JoinRoom(props: IJoinRoomProps) {
           <svg width="20" height="20" fill="currentColor" className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
             <path fillRule="evenodd" clipRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
           </svg>
-          <input type="text" aria-label="Room" placeholder="Room ID" value={roomName} onChange={handleRoomNameChange} className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" />
+          <input type="text" aria-label="Room" placeholder="Room ID" value={inputValue} onChange={handleInputValueChange} className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" />
         </div>
 
         <button type="submit" disabled={isJoining} className="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
@@ -54,6 +57,10 @@ export function JoinRoom(props: IJoinRoomProps) {
           {isJoining ? 'Joining...' : 'Join'}
         </button>
       </form>
+
+      <div>
+        {`Room name: ${isInRoom ? `${roomName}` : ''}`}
+      </div>
 
     </div>
   )
