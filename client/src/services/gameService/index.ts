42ff9eb3ffd965cba0ec9ecaf2e7e061555fbac0
Gabriel Marcano <gabrielmarcano120@gmail.com>
2023-01-05 09:58:44 -0400

    return roomid as string

--- client/src/services/gameService/index.ts
+++ client/src/services/gameService/index.ts
@@ -3,7 +3,7 @@ import { Socket } from "socket.io-client"
 class GameService {
   public async joinGameRoom(socket: Socket, roomId: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
-      socket.emit("join_game", { roomId })
+      socket.emit("join_game", roomId)
       socket.on('room_joined', () => resolve(true))
       socket.on("room_join_error", ({ error }) => reject(error))
     })
