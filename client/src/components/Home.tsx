e1ab1fe46466559546fa54b563da6604b9aee354
Gabriel Marcano <gabrielmarcano120@gmail.com>
2023-01-07 14:43:11 -0400

    client: show home only if socket loaded

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -4,7 +4,9 @@ import { useNavigate } from 'react-router-dom';
 import JoinRoom from './JoinRoom'
 import SocketService from '../services/SocketService';
 
-interface IHome { }
+interface IHome {
+  isLoading: boolean
+}
 
 export default function Home(props: IHome) {
   const navigate = useNavigate()
@@ -20,25 +22,32 @@ export default function Home(props: IHome) {
 
   return (
     <>
-      <form className="home__container" onSubmit={handleSubmit}>
-        <h2 className="home__header">Sign in to Open Chat</h2>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          minLength={6}
-          name="username"
-          id="username"
-          className="username__input"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <button className="home__cta">SIGN IN</button>
-      </form>
+      {props.isLoading ? 
+      "Is loading" 
+      : (
+          <div>
+            <form className="home__container" onSubmit={handleSubmit}>
+              <h2 className="home__header">Sign in to Open Chat</h2>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                minLength={6}
+                name="username"
+                id="username"
+                className="username__input"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+              <button className="home__cta">SIGN IN</button>
+            </form>
 
-      <div className="flex flex-col justify-center items-center">
-        <h1 className='mt-5 underline text-4xl'>Guess Who Game</h1>
-        {SocketService.socket ? <JoinRoom></JoinRoom> : "Waiting for connection..."}
-      </div>
+            <div className="flex flex-col justify-center items-center">
+              <h1 className='mt-5 underline text-4xl'>Guess Who Game</h1>
+              {<JoinRoom></JoinRoom>}
+            </div>
+          </div>
+        )
+      }
     </>
   )
 }
